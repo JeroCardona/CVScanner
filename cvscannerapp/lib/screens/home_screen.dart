4376fd20d92a928b4c4b1c405de8429deae7d52c
Vero <vzapatav@eafit.edu.co>
2025-03-04 23:11:34 -0500

    Raw v1.1

--- cvscannerapp/lib/screens/home_screen.dart
+++ cvscannerapp/lib/screens/home_screen.dart
@@ -4,27 +4,48 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
-              'Bienvenido a la pantalla Home',
+              'Bienvenido a CVScanner',
+              textAlign: TextAlign.center,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                Navigator.pushNamed(context, '/');
+                Navigator.pushNamed(context, '/scanFile');
               },
-              child: Text('Volver al Index'),
+              child: Text('📂 Escanear hoja de vida desde archivos'),
             ),
+            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
-                Navigator.pushNamed(context, '/login');
+                Navigator.pushNamed(context, '/scanCamera');
               },
-              child: Text('Cerrar sesión'),
+              child: Text('📸 Escanear hoja de vida desde cámara'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/previousScans');
+              },
+              child: Text('📄 Ver hojas de vida anteriores'),
             ),
           ],
         ),
